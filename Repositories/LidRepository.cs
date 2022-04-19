7ca30b453fa35297e4fd31b2cf5bbf1b28133dfc
Anton Labeeuw <anton.labeeuw@student.howest.be>
2022-04-19 22:53:56 +0200

    Tak repository gemaakt

--- Repositories/LidRepository.cs
+++ Repositories/LidRepository.cs
@@ -1,9 +1,20 @@
 namespace Leden.API.Repositories;
 
-public class LidRepository{
+public interface ILidRepository
+{
+    Task<Lid> AddLid(Lid newLid);
+    Task<List<Lid>> GetAllLeden();
+    Task<List<Lid>> GetLedenByGroepId(Guid GroepId);
+    Task<List<Lid>> GetLedenByTakId(Guid TakId);
+    Task<Lid> GetLid(Guid id);
+}
+
+public class LidRepository : ILidRepository
+{
     private readonly IMongoContext _context;
 
-    public LidRepository(IMongoContext context){
+    public LidRepository(IMongoContext context)
+    {
         _context = context;
     }
 
@@ -15,7 +26,8 @@ public class LidRepository{
 
     public async Task<List<Lid>> GetLedenByGroepId(Guid GroepId) => await _context.LidCollection.Find(l => l.Groep.GroepId == GroepId).ToListAsync();
 
-    public async Task<Lid> AddLid(Lid newLid){
+    public async Task<Lid> AddLid(Lid newLid)
+    {
         await _context.LidCollection.InsertOneAsync(newLid);
         return newLid;
     }
